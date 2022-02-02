49b85c24f7cbf05b24123516be31545744de7801
rafaeelnunes <rafaelnfsq@gmail.com>
2022-02-02 19:03:16 -0300

    POST request on the /login route and creation of the userLoginShema f…

--- app.js
+++ app.js
@@ -30,13 +30,14 @@ const userLoginSchema = joi.object({
 
 app.post("/register", async (req, res) => {
   const user = req.body;
-  const passwordHashed = bcrypt.hashSync(user.password, 10);
+
   const validation = userSchema.validate(user, { abortEarly: true });
 
   if (validation.error) {
     return res.status(422).send(validation.error.details);
   }
   try {
+    const passwordHashed = bcrypt.hashSync(user.password, 10);
     const userExist = await db
       .collection("users")
       .findOne({ $or: [{ name: user.name }, { email: user.email }] });
@@ -85,14 +86,36 @@ app.post("/login", async (req, res) => {
         userId: user._id,
         token,
       });
-      res.send(token);
+      return res.send({ name: user.name, id: user._id, token });
     }
+    res.status(403).send("Senha inválida");
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
 });
 
+app.get("/home", async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) return res.sendStatus(401);
+
+    const user = await db.collection("users").findOne({ _id: session.userId });
+
+    if (user) {
+      delete user.password;
+      res.send(user);
+    } else res.sendStatus(401);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 app.listen(5000, () => {
   console.log("servidor rodando na porta 5000...");
 });
